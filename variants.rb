3f14aece8c5afe5a9c6808dfd142f89603747e02
Earlopain <14981592+Earlopain@users.noreply.github.com>
2025-02-18 14:12:38 +0100

    One more time

--- variants.rb
+++ variants.rb
@@ -4,4 +4,8 @@ require "json"
 
 json = JSON.parse(File.read("docker-ruby/versions.json"))
 latest_release = json.compact.keys.max_by(&:to_f)
+
+# Until irb releases a new version (current 1.15.1)
+json[latest_release]["variants"].delete_if { |v| v.include?("bullseye")  }
+
 puts json[latest_release]["variants"].to_json
